bf1a33cf384da6975b064c20cd6a2fdd08ac5a1e
Luis F. Tello A. <seizin00@tuta.io>
2026-02-27 19:14:41 -0500

    Mejora código

--- AdventJS/2025/reto 7/7.py
+++ AdventJS/2025/reto 7/7.py
@@ -4,10 +4,9 @@ def draw_tree(height, ornament, frequency):
   for i, k in enumerate(range(height, 0, -1)):
     tree.append(" " * (k - 1))
     for j in range(2*i + 1):
-      tree.append(ornament if c % frequency == 0 else "*")
-		  c += 1
+    	tree.append(ornament if c % frequency == 0 else "*")
+    	c += 1
     tree.append("\n")
   
-  tree.pop()
-  tree.append("\n" + " " * (height - 1) + "#")
+  tree.append(" " * (height - 1) + "#")
   return "".join(tree)
